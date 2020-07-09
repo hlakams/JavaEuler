a6aab55706b57059b880ff74a2368978cf17321d
Harsha Lakamsani <harsha.lakamsani@gmail.com>
2020-07-08 23:31:02 -0700

    Updated P01, P11

--- src/euler/P01.java
+++ src/euler/P01.java
@@ -1,10 +1,10 @@
 package euler;
 
-public class P1 {
+public class P01 {
 	public static void main(String[] args) {
 		int i = 0;
 		int sum = 0;
-		for(i = 0; i < 10; i++) {
+		for(i = 0; i < 1000; i++) {
 			if(i % 3 == 0 || i % 5 == 0 || (i % 3 == 0 && i % 5 == 0)) {
 				sum += i;
 			}
